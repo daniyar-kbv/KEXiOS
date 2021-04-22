a76f62b9de905934fa4ef66a2df39556fbe6bc06
Arystan Amanzholov <amanzholov.arystan@gmail.com>
2021-04-22 14:24:27 +0600

    fix: structure

---
+++ SalamBro/Presentation/Features/RateOrder/RateController.swift
@@ -0,0 +1,28 @@
+//
+//  RateController.swift
+//  SalamBro
+//
+//  Created by Arystan on 4/22/21.
+//
+
+import UIKit
+
+class RateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
