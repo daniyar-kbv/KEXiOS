04276a67d7d324cf786022df8ef2f39e937c2238
Arystan Amanzholov <amanzholov.arystan@gmail.com>
2021-05-05 14:31:24 +0600

    feat: support view

--- SalamBro/Presentation/Features/Profile/ProfileController.swift
+++ SalamBro/Presentation/Features/Profile/ProfileController.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 class ProfileController: UIViewController {
-
     fileprivate lazy var rootView = ProfileView(delegate: self)
-    
+
     override func loadView() {
         view = rootView
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -22,25 +21,24 @@ class ProfileController: UIViewController {
 
     func setupUI() {
         parent?.navigationController?.title = L10n.Profile.NavigationBar.title
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
 }
 
 extension ProfileController: ProfileViewDelegate {
     func changeName() {
-        self.navigationController?.pushViewController(ChangeNameController(), animated: true)
+        navigationController?.pushViewController(ChangeNameController(), animated: true)
     }
-    
+
     func logout() {
         print("logout action")
     }
 }
 
 extension ProfileController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         2
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = profileItems[indexPath.row]
@@ -51,14 +49,12 @@ extension ProfileController: UITableViewDelegate, UITableViewDataSource {
         cell.layoutMargins = UIEdgeInsets.zero
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
-            self.navigationController?.pushViewController(OrderHistoryController(), animated: true)
+            navigationController?.pushViewController(OrderHistoryController(), animated: true)
         } else {
-            self.navigationController?.pushViewController(ChangeLanguageController(), animated: true)
+            navigationController?.pushViewController(ChangeLanguageController(), animated: true)
         }
     }
-    
 }
-
