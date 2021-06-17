333af1c606ff43b2aab07cf8a01ecf5d4bcc0c05
Tastandiyeva Meruyert <merutas@mail.ru>
2021-06-18 00:21:52 +0600

    fix: rate

--- SalamBro/Presentation/Features/RateOrder/RateController.swift
+++ SalamBro/Presentation/Features/RateOrder/RateController.swift
@@ -2,13 +2,116 @@
 //  RateController.swift
 //  SalamBro
 //
-//  Created by Arystan on 4/22/21.
+//  Created by Meruyert Tastandiyeva on 6/16/21.
 //
 
 import Cosmos
+import SnapKit
 import UIKit
 
-class RateController: ViewController {
+class RateController: UIViewController {
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+
+    private lazy var cosmosView: CosmosView = {
+        let view = CosmosView()
+        view.settings.filledColor = .kexRed
+        view.settings.filledBorderColor = .kexRed
+        view.settings.filledBorderWidth = 0
+        view.settings.emptyBorderColor = .kexRed
+        view.settings.emptyColor = .white
+        view.settings.starSize = 30
+        view.settings.starMargin = 16
+        view.settings.emptyImage = UIImage(named: "star.empty")
+        view.settings.filledImage = UIImage(named: "star.filled")
+        view.rating = 0
+        return view
+    }()
+
+    private lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 19, weight: .medium)
+        label.text = L10n.RateOrder.title
+        label.textColor = .darkGray
+        label.baselineAdjustment = .alignBaselines
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        label.contentMode = .left
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var suggestionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.text = L10n.RateOrder.description
+        label.textColor = .darkGray
+        label.baselineAdjustment = .alignBaselines
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        label.contentMode = .left
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: .init()
+        )
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+
+    private lazy var commentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.cornerRadius = 10
+        return view
+    }()
+
+    private lazy var commentTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.attributedPlaceholder = NSAttributedString(
+            string: L10n.RateOrder.CommentaryField.placeholder,
+            attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .medium)]
+        )
+        textfield.borderStyle = .none
+        textfield.clearButtonMode = .never
+        textfield.minimumFontSize = 17
+        textfield.adjustsFontSizeToFitWidth = true
+        textfield.contentHorizontalAlignment = .left
+        textfield.contentVerticalAlignment = .center
+        textfield.isUserInteractionEnabled = false
+        return textfield
+    }()
+
+    private lazy var sendButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(L10n.RateOrder.SubmitButton.title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .mildBlue
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.cornerRadius = 10
+        button.contentHorizontalAlignment = .center
+        button.contentVerticalAlignment = .center
+        button.adjustsImageWhenHighlighted = true
+        button.adjustsImageWhenDisabled = true
+        button.addTarget(self, action: #selector(dismissVC), for: .allTouchEvents)
+        return button
+    }()
+
+    var collectionViewHeightConstraint: Constraint?
+
+    lazy var shadow: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.layer.opacity = 0.7
+        view.isHidden = true
+        return view
+    }()
+
     var data: [String] = []
 
     var arrayStar13: [String] = [L10n.RateOrder.Cell.CourierWork.text, L10n.RateOrder.Cell.GivenTime.text, L10n.RateOrder.Cell.CourierNotFoundClient.text, L10n.RateOrder.Cell.FoodIsMissing.text, L10n.RateOrder.Cell.FoodIsCold.text]
@@ -19,48 +122,85 @@ class RateController: ViewController {
 
     var selectedItems: [String] = []
 
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var ratingView: CosmosView!
-    @IBOutlet var subtitleFirstLabel: UILabel!
-    @IBOutlet var subtitleSecondLabel: UILabel!
-    @IBOutlet var collectionView: UICollectionView!
-    @IBOutlet var commentaryField: UITextField!
-    @IBOutlet var commentaryView: UIView!
-    @IBOutlet var button: UIButton!
-    @IBOutlet var collectionViewHeightConstraint: NSLayoutConstraint!
-    @IBOutlet private var scrollView: UIScrollView!
-
     lazy var commentarySheetVC = CommentarySheetController()
 
-    lazy var shadow: UIView = {
-        let view = UIView()
-        view.backgroundColor = .darkGray
-        view.layer.opacity = 0.7
-        view.isHidden = true
-        return view
-    }()
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-        setupConstraints()
+        layoutUI()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        collectionViewHeightConstraint.constant = collectionView.collectionViewLayout.collectionViewContentSize.height
+        collectionViewHeightConstraint?.update(offset: collectionView.contentSize.height)
     }
 
-    private func setupViews() {
-        view.addSubview(shadow)
-        titleLabel.text = L10n.RateOrder.title
-        subtitleSecondLabel.text = L10n.RateOrder.description
-        button.setTitle(L10n.RateOrder.SubmitButton.title, for: .normal)
-        commentaryField.attributedPlaceholder = NSAttributedString(
-            string: L10n.RateOrder.CommentaryField.placeholder,
-            attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .medium)]
-        )
-        ratingView.didFinishTouchingCosmos = didFinishTouchRating
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.backgroundColor = .arcticWhite
+        navigationController?.navigationBar.tintColor = .kexRed
+        navigationController?.navigationItem.title = L10n.RateOrder.title
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "chevron.left"), style: .plain, target: self, action: #selector(goBack))
+    }
+}
+
+extension RateController {
+    private func layoutUI() {
+        view.backgroundColor = .arcticWhite
+
+        [scrollView, sendButton].forEach {
+            view.addSubview($0)
+        }
+
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+            $0.width.equalTo(scrollView)
+        }
+
+        [cosmosView, questionLabel, suggestionLabel, collectionView, commentView].forEach {
+            contentView.addSubview($0)
+        }
+
+        cosmosView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(100)
+            $0.trailing.equalToSuperview().offset(-100)
+            $0.top.equalToSuperview().offset(34)
+            $0.centerX.equalTo(questionLabel.snp.centerX)
+        }
+
+        questionLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.top.equalTo(cosmosView.snp.bottom).offset(32)
+        }
+
+        suggestionLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.top.equalTo(questionLabel.snp.bottom).offset(8)
+        }
+
+        collectionView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.top.equalTo(suggestionLabel.snp.bottom).offset(8)
+            collectionViewHeightConstraint = $0.height.equalTo(0).constraint
+        }
 
         let layout = UICollectionViewCenterLayout()
         layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
@@ -69,61 +209,97 @@ class RateController: ViewController {
         layout.scrollDirection = .vertical
 
         collectionView.collectionViewLayout = layout
+        collectionView.allowsMultipleSelection = true
         collectionView.register(cellType: RateItemCell.self)
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.allowsMultipleSelection = true
+
+        commentView.addSubview(commentTextField)
+
+        commentView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.top.equalTo(collectionView.snp.bottom).offset(8)
+            $0.bottom.equalToSuperview().offset(-20)
+            $0.height.equalTo(50)
+        }
+
+        commentTextField.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+
+        sendButton.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(24)
+            $0.right.equalToSuperview().offset(-24)
+            $0.bottom.equalToSuperview().offset(-16)
+            $0.height.equalTo(43)
+        }
+
+        cosmosView.didFinishTouchingCosmos = didFinishTouchRating
 
         let tapCommentary = UITapGestureRecognizer(target: self, action: #selector(commentaryViewTapped))
-        commentaryView.addGestureRecognizer(tapCommentary)
+        commentView.addGestureRecognizer(tapCommentary)
 
-        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.safeAreaInsets.bottom - 230, right: 0)
-    }
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.safeAreaInsets.bottom + 50, right: 0)
 
-    func setupConstraints() {
+        view.addSubview(shadow)
         shadow.snp.makeConstraints {
             $0.top.left.right.bottom.equalToSuperview()
         }
     }
 
     private func didFinishTouchRating(_ rating: Double) {
-        button.isEnabled = true
-        button.backgroundColor = .kexRed
+        sendButton.isEnabled = true
+        sendButton.backgroundColor = .kexRed
         switch rating {
         case 1.0 ..< 2.0:
-            subtitleFirstLabel.text = L10n.RateOrder.BadRate.title
-            subtitleSecondLabel.text = L10n.RateOrder.BadRate.subtitle
+            questionLabel.text = L10n.RateOrder.BadRate.title
+            suggestionLabel.text = L10n.RateOrder.BadRate.subtitle
             data = arrayStar13
         case 2.0 ..< 3.0:
-            subtitleFirstLabel.text = L10n.RateOrder.BadRate.title
-            subtitleSecondLabel.text = L10n.RateOrder.BadRate.subtitle
+            questionLabel.text = L10n.RateOrder.BadRate.title
+            suggestionLabel.text = L10n.RateOrder.BadRate.subtitle
             data = arrayStar13
         case 3.0 ..< 4.0:
-            subtitleFirstLabel.text = L10n.RateOrder.AverageRate.title
-            subtitleSecondLabel.text = L10n.RateOrder.AverageRate.title
+            questionLabel.text = L10n.RateOrder.AverageRate.title
+            suggestionLabel.text = L10n.RateOrder.AverageRate.title
             data = arrayStar13
         case 4.0 ..< 5.0:
-            subtitleFirstLabel.text = L10n.RateOrder.GoodRate.title
-            subtitleSecondLabel.text = L10n.RateOrder.GoodRate.subtitle
+            questionLabel.text = L10n.RateOrder.GoodRate.title
+            suggestionLabel.text = L10n.RateOrder.GoodRate.subtitle
             data = arrayStar4
         case 5.0:
-            subtitleFirstLabel.text = L10n.RateOrder.ExcellentRate.title
-            subtitleSecondLabel.text = L10n.RateOrder.ExcellentRate.subtitle
+            questionLabel.text = L10n.RateOrder.ExcellentRate.title
+            suggestionLabel.text = L10n.RateOrder.ExcellentRate.subtitle
             data = arrayStar5
         default:
-            subtitleFirstLabel.text = nil
-            subtitleSecondLabel.text = nil
+            questionLabel.text = nil
+            suggestionLabel.text = nil
         }
         collectionView.reloadData()
         collectionView.layoutIfNeeded()
     }
+}
 
-    @IBAction func backButtonTapped(_: UIButton) {
-        dismiss(animated: true)
+extension RateController {
+    @objc func dismissVC() {
+        print("dismiss")
     }
 
     @objc func commentaryViewTapped() {
         showCommentarySheet()
+    }
+
+    @objc func goBack() {
+        if navigationController?.presentingViewController != nil {
+            dismiss(animated: true, completion: nil)
+            return
+        }
+
+        navigationController?.popViewController(animated: true)
     }
 }
 
@@ -195,7 +371,7 @@ extension RateController: MapDelegate {
     }
 
     func passCommentary(text: String) {
-        commentaryField.text = text
+        commentTextField.text = text
     }
 
     func reverseGeocoding(searchQuery _: String, title _: String) {
@@ -205,8 +381,10 @@ extension RateController: MapDelegate {
     func mapShadow(toggle: Bool) {
         if toggle {
             shadow.isHidden = false
+            navigationController?.setNavigationBarHidden(true, animated: false)
         } else {
             shadow.isHidden = true
+            navigationController?.setNavigationBarHidden(false, animated: false)
         }
     }
 }
