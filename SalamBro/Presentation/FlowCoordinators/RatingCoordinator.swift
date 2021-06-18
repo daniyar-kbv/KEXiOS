82e3fd45555491f9f9dc0fccd5a93c3ec354bf51
daniyarka <daniyar.kbv@gmail.com>
2021-06-18 18:06:42 +0600

    fix: rating coordinator renamed for merge

--- SalamBro/Presentation/FlowCoordinators/RatingCoordinator.swift
+++ SalamBro/Presentation/FlowCoordinators/RatingCoordinator.swift
@@ -6,41 +6,42 @@
 //
 
 import Foundation
-import UIKit
-import RxSwift
 import RxCocoa
+import RxSwift
+import UIKit
 
 final class PromotionsCoordinator {
     private let disposeBag = DisposeBag()
     private var navigationController: UINavigationController
     private var pagesFactory: PromotionsPagesFactory
-    
+
     var didFinish: (() -> Void)?
 
     init(navigationController: UINavigationController,
-         pagesFactory: PromotionsPagesFactory) {
+         pagesFactory: PromotionsPagesFactory)
+    {
         self.navigationController = navigationController
         self.pagesFactory = pagesFactory
     }
 
     func start(promotionURL: URL, infoURL: URL?) {
         let promotionPage = pagesFactory.makePromotionsPage(promotionURL: promotionURL, infoURL: infoURL)
-        
+
         promotionPage.outputs.didTerminate.subscribe(onNext: { [weak self] in
             self?.didFinish?()
         }).disposed(by: disposeBag)
-        
+
         promotionPage.outputs.toInfo.subscribe(onNext: { [weak self] url in
             self?.openPromotionInfo(url: url)
         }).disposed(by: disposeBag)
-        
+
         promotionPage.hidesBottomBarWhenPushed = true
         navigationController.pushViewController(promotionPage, animated: true)
     }
-    
+
     private func openPromotionInfo(url: URL) {
         let promotionInfoPage = pagesFactory.makePromotionsInfoPage(url: url)
-        
+
         navigationController.present(promotionInfoPage, animated: true)
     }
 }
