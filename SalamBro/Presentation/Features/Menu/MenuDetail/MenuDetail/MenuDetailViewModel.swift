58d53ef0d6e6d1f5ef452f9cb5d6dc3553e4b9c8
daniyarka <daniyar.kbv@gmail.com>
2021-08-20 20:48:09 +0600

    fix: menu didn't update on address change

--- SalamBro/Presentation/Features/Menu/MenuDetail/MenuDetail/MenuDetailViewModel.swift
+++ SalamBro/Presentation/Features/Menu/MenuDetail/MenuDetail/MenuDetailViewModel.swift
@@ -135,7 +135,8 @@ extension MenuDetailViewModelImpl {
 
     private func check() {
         guard let isComplete = position?.modifierGroups
-            .filter({ $0.isRequired && $0.selectedModifiers.count < $0.minAmount })
+            .filter({ $0.isRequired &&
+                    $0.selectedModifiers.map { $0.itemCount }.reduce(0, +) < $0.minAmount })
             .isEmpty
         else { return }
         outputs.isComplete.accept(isComplete)
