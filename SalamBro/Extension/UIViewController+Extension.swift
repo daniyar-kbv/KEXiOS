aedda4d9e16707ff1cf2ac6253ee762d25198c99
Меруерт <merutas@mail.ru>
2021-09-06 18:09:42 +0600

    fix: window uprising

--- SalamBro/Extension/UIViewController+Extension.swift
+++ SalamBro/Extension/UIViewController+Extension.swift
@@ -18,3 +18,27 @@ extension UIViewController {
         view.endEditing(true)
     }
 }
+
+extension UIViewController {
+    func checkForViewHeight() {
+        if view.frame.height <= 568 {
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    }
+
+    func removeObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIApplication.shared.keyWindow?.frame.origin.y -= keyboardSize.height / 2
+        }
+    }
+
+    @objc func keyboardWillHide(notification _: NSNotification) {
+        UIApplication.shared.keyWindow?.frame.origin.y = 0
+    }
+}
--- SalamBro/Presentation/Features/Authorization/Verification/VerificationController.swift
+++ SalamBro/Presentation/Features/Authorization/Verification/VerificationController.swift
@@ -22,6 +22,10 @@ final class VerificationController: UIViewController, LoaderDisplayable {
         super.init(nibName: nil, bundle: nil)
     }
 
+    deinit {
+        removeObservers()
+    }
+
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -30,6 +34,7 @@ final class VerificationController: UIViewController, LoaderDisplayable {
     override func viewDidLoad() {
         super.viewDidLoad()
         bindViewModel()
+        checkForViewHeight()
     }
 
     override func loadView() {
@@ -88,7 +93,6 @@ final class VerificationController: UIViewController, LoaderDisplayable {
     }
 
     private func reload() {
-        print("reload()")
         rootView?.reload(for: true)
     }
 
