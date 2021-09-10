8e86bcfa8ecd814c8f995a00c42c7b3b52b990aa
daniyarka <daniyar.kbv@gmail.com>
2021-09-10 11:58:31 +0600

    fix: adding yandex api

---
+++ SalamBro/NetworkLayer/API/YandexAPI.swift
@@ -0,0 +1,47 @@
+//
+//  YandexAPI.swift
+//  SalamBro
+//
+//  Created by Dan on 9/10/21.
+//
+
+import Moya
+
+enum RateAPI {
+    case geocode()
+}
+
+extension RateAPI: TargetType {
+    var baseURL: URL {
+        return Constants.URLs.APIBase.dev
+    }
+
+    var path: String {
+        switch self {
+        case .getRates: return "orders/ratestars/"
+        case .saveUserRate: return "orders/rates/"
+        }
+    }
+
+    var method: Method {
+        switch self {
+        case .getRates: return .get
+        case .saveUserRate: return .post
+        }
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+
+    var task: Task {
+        switch self {
+        case .getRates: return .requestPlain
+        case let .saveUserRate(dto): return .requestJSONEncodable(dto)
+        }
+    }
+
+    var headers: [String: String]? {
+        return nil
+    }
+}
---
+++ SalamBro/NetworkLayer/Models/DTO/YandexDTOs.swift
@@ -0,0 +1,10 @@
+//
+//  YandexDTOs.swift
+//  SalamBro
+//
+//  Created by Dan on 9/10/21.
+//
+
+import Foundation
+
+struct ReverseGeocodeDTO {}
